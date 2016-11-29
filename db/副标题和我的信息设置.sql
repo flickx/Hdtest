6a2451e11b22af55205e3cae657ec8f84482e0e2
yuanw <user@ftoul-pc40>
2016-11-29 09:11:25 +0800

    sql

--- db/副标题和我的信息设置.sql
+++ db/副标题和我的信息设置.sql
@@ -4,3 +4,9 @@ ADD COLUMN `goods_id`  varchar(32) NULL COMMENT '关联商品id' AFTER `shop_id`;
 ALTER TABLE `goods`
 DROP COLUMN `subtitle`,
 ADD COLUMN `subtitle`  varchar(100) NULL COMMENT '副标题' AFTER `title`;
+ALTER TABLE `user`
+DROP COLUMN `nickname`,
+DROP COLUMN `birth`,
+ADD COLUMN `nickname`  varchar(32) NULL AFTER `source`,
+ADD COLUMN `birth`  varchar(32) NULL AFTER `name`;
+
