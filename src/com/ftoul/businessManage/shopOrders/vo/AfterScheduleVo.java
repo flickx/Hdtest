426d8b246c82e5823236c5172e879084805da97f
hud <hud@ttl.com>
2016-12-02 15:16:00 +0800

    将订单功能移到商家后台

---
+++ src/com/ftoul/businessManage/shopOrders/vo/AfterScheduleVo.java
@@ -0,0 +1,238 @@
+package com.ftoul.businessManage.shopOrders.vo;
+
+import java.util.List;
+
+import com.ftoul.po.AfterSchedule;
+
+public class AfterScheduleVo {
+	
+	private String id;
+	private String serviceCode;
+	private String scheduleStatic;
+	private String logCompany;
+	private String logOdd;
+	private String tel;
+	private String type;
+	private String reason;
+	private String backPrice;
+	private String num;
+	private String orderNumber;
+	private String orderStatic;
+	private String orderTime;
+	private String createTime;
+	private List<AfterSchedule> list;
+	
+	private String company;
+	private String odd;
+	private String consignee;
+	private String address;
+	private String consigneeTel;
+	private String mobil;
+	
+	private String goodsId;
+	private String goodsPicSrcs;
+	private String goodsName;
+	private String param;
+	private String number;
+	private String totalPrice;
+	private String salePrice;
+	private String costPrice;//进货价
+	private String canal;//进货商
+	private String sku;
+	
+	private List<Object> logList;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getServiceCode() {
+		return serviceCode;
+	}
+	public void setServiceCode(String serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+	public String getScheduleStatic() {
+		return scheduleStatic;
+	}
+	public void setScheduleStatic(String scheduleStatic) {
+		this.scheduleStatic = scheduleStatic;
+	}
+	public String getLogCompany() {
+		return logCompany;
+	}
+	public void setLogCompany(String logCompany) {
+		this.logCompany = logCompany;
+	}
+	public String getLogOdd() {
+		return logOdd;
+	}
+	public void setLogOdd(String logOdd) {
+		this.logOdd = logOdd;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getBackPrice() {
+		return backPrice;
+	}
+	public void setBackPrice(String backPrice) {
+		this.backPrice = backPrice;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getOrderStatic() {
+		return orderStatic;
+	}
+	public void setOrderStatic(String orderStatic) {
+		this.orderStatic = orderStatic;
+	}
+	public String getOrderTime() {
+		return orderTime;
+	}
+	public void setOrderTime(String orderTime) {
+		this.orderTime = orderTime;
+	}
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getOdd() {
+		return odd;
+	}
+	public void setOdd(String odd) {
+		this.odd = odd;
+	}
+	public String getConsignee() {
+		return consignee;
+	}
+	public void setConsignee(String consignee) {
+		this.consignee = consignee;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getConsigneeTel() {
+		return consigneeTel;
+	}
+	public void setConsigneeTel(String consigneeTel) {
+		this.consigneeTel = consigneeTel;
+	}
+	public String getMobil() {
+		return mobil;
+	}
+	public void setMobil(String mobil) {
+		this.mobil = mobil;
+	}
+	public String getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(String goodsId) {
+		this.goodsId = goodsId;
+	}
+	public String getGoodsPicSrcs() {
+		return goodsPicSrcs;
+	}
+	public void setGoodsPicSrcs(String goodsPicSrcs) {
+		this.goodsPicSrcs = goodsPicSrcs;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public String getParam() {
+		return param;
+	}
+	public void setParam(String param) {
+		this.param = param;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(String totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public String getCostPrice() {
+		return costPrice;
+	}
+	public void setCostPrice(String costPrice) {
+		this.costPrice = costPrice;
+	}
+	public String getCanal() {
+		return canal;
+	}
+	public void setCanal(String canal) {
+		this.canal = canal;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getSalePrice() {
+		return salePrice;
+	}
+	public void setSalePrice(String salePrice) {
+		this.salePrice = salePrice;
+	}
+	public List<Object> getLogList() {
+		return logList;
+	}
+	public void setLogList(List<Object> logList) {
+		this.logList = logList;
+	}
+	public List<AfterSchedule> getList() {
+		return list;
+	}
+	public void setList(List<AfterSchedule> list) {
+		this.list = list;
+	}
+	
+}
