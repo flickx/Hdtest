467697de25d3f2bac1b68d7694d12c5836ee50e5
lid <dean118@126.com>
2016-12-20 17:03:22 +0800

    1

--- src/com/ftoul/web/manage/user/action/WebUserAction.java
+++ src/com/ftoul/web/manage/user/action/WebUserAction.java
@@ -3,12 +3,6 @@
  */
 package com.ftoul.web.manage.user.action;
 
-import java.util.Enumeration;
-
-import javax.servlet.http.HttpServletRequest;
-
-import net.sf.json.JSONObject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
