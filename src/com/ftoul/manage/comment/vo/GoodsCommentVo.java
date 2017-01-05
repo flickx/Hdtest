dc88dc4dd6ce562e28e5063674a07fe9c941af37
hud <hud@ttl.com>
2017-01-05 17:39:55 +0800

    评价管理功能

---
+++ src/com/ftoul/manage/comment/vo/GoodsCommentVo.java
@@ -0,0 +1,93 @@
+package com.ftoul.manage.comment.vo;
+
+
+public class GoodsCommentVo {
+	
+	private String id;
+	private String goodsTitle;
+	private String commentCode;
+	private String userName;
+	private String commentType;
+	private String commentContent;
+	private String ip;
+	private String commentTime;
+	private String isShow;
+	private String state;
+	private String picSrc;
+	private String star;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getGoodsTitle() {
+		return goodsTitle;
+	}
+	public void setGoodsTitle(String goodsTitle) {
+		this.goodsTitle = goodsTitle;
+	}
+	public String getCommentCode() {
+		return commentCode;
+	}
+	public void setCommentCode(String commentCode) {
+		this.commentCode = commentCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getCommentType() {
+		return commentType;
+	}
+	public void setCommentType(String commentType) {
+		this.commentType = commentType;
+	}
+	public String getCommentContent() {
+		return commentContent;
+	}
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getCommentTime() {
+		return commentTime;
+	}
+	public void setCommentTime(String commentTime) {
+		this.commentTime = commentTime;
+	}
+	public String getIsShow() {
+		return isShow;
+	}
+	public void setIsShow(String isShow) {
+		this.isShow = isShow;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPicSrc() {
+		return picSrc;
+	}
+	public void setPicSrc(String picSrc) {
+		this.picSrc = picSrc;
+	}
+	public String getStar() {
+		return star;
+	}
+	public void setStar(String star) {
+		this.star = star;
+	}
+	
+	
+
+}
