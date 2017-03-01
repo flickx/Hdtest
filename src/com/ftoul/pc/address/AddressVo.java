b7617ea10f2bf51689e36355599686273d418f61
hud <hud@ttl.com>
2017-03-01 10:56:28 +0800

    优化地址

---
+++ src/com/ftoul/pc/address/AddressVo.java
@@ -0,0 +1,50 @@
+package com.ftoul.pc.address;
+
+public class AddressVo {
+
+	private String id;
+	private String consigee;
+	private String tel;
+	private String name;
+	private String address;
+	private String isDefault;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getConsigee() {
+		return consigee;
+	}
+	public void setConsigee(String consigee) {
+		this.consigee = consigee;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getIsDefault() {
+		return isDefault;
+	}
+	public void setIsDefault(String isDefault) {
+		this.isDefault = isDefault;
+	}
+	
+	
+}
